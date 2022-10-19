493cc0df5bfb590b27e9de649ce89d3443c0b02c
rodrigocqb <rodrigocortibarros@gmail.com>
2022-10-19 15:13:32 -0300

    feat: Included auth middleware in user posts GET request

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getUserPosts } from "../controllers/users.controller.js";
+import { auth } from "../middlewares/authorization.middleware.js";
 
 const router = express.Router();
 
-router.get("/user/:id", getUserPosts);
+router.get("/user/:id", auth, getUserPosts);
 
 export default router;
