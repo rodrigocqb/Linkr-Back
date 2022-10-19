a9b41b9648d184dfc37068d06d69d63beb614f8e
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-10-19 14:32:31 -0300

    feat: hashtag controller

---
+++ src/controllers/posts.controller.js
@@ -0,0 +1,64 @@
+import connection from "../database/database.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const newPost = async (req, res) => {
+  const { url, message } = res.locals.body;
+  const { userId } = res.locals.session;
+  const { hashtags } = res.locals;
+};
+
+const testUser = async (req, res) => {
+  const { email, username, password, image } = req.body;
+
+  if (password === undefined) {
+    return res.status(404).send({ message: "Password is undefined" });
+  }
+
+  const passHash = bcrypt.hashSync(password, 12);
+
+  try {
+    await connection.query(
+      `INSERT INTO users (email, username, password, image) VALUES ($1, $2, $3, $4);`,
+      [email, username, passHash, image]
+    );
+    return res.status(201).send({ message: "User created" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error });
+  }
+};
+
+const tesLogin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const userIsValid = await connection.query(
+      `SELECT * FROM users where email = $1;`,
+      [email]
+    );
+    if (userIsValid.rowCount == 0) {
+      return res.status(401).send({ error: "Invalid email or password." });
+    }
+
+    const user = userIsValid.rows[0];
+    const passwordValid = bcrypt.compareSync(password, user.password);
+
+    if (!passwordValid) {
+      return res.status(401).send({ error: "Invalid email or password." });
+    }
+
+    const data = { userId: user.id };
+    const token = jwt.sign(data, process.env.JWT_SECRET);
+
+    await connection.query(
+      `INSERT INTO sessions (token, user_id) VALUES ($1, $2);`,
+      [token, user.id]
+    );
+
+    return res.status(200).send({ token });
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
+
+export { testUser, tesLogin, newPost };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+import { postRouter } from "./routers/posts.router.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(postRouter);
 
 export default app;
