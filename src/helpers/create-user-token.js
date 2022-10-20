b7ee9b5ebe4b00ba1ca7c388fb3e3e1586c7340a
rodrigocqb <rodrigocortibarros@gmail.com>
2022-10-19 22:27:40 -0300

    fix: Fixed user/:id and /timeline queries

--- src/helpers/create-user-token.js
+++ src/helpers/create-user-token.js
@@ -3,21 +3,21 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
-const createUserToken = async(user, req, res) => {
+const createUserToken = async (user, req, res) => {
+  const token = jwt.sign(
+    {
+      id: user.id,
+    },
+    process.env.JWT_SECRET
+  );
 
-    // Criando o token
-    const token = jwt.sign({
-        id: user.id
-    }, `${process.env.TOKEN_SECRET}`);
-
-    res.status(200).json({
-        message: "Authenticated user!",
-        id: user.id,
-        username: user.username,
-        image: user.image,
-        token
-    });
-
-}
+  res.status(200).json({
+    message: "Authenticated user!",
+    id: user.id,
+    username: user.username,
+    image: user.image,
+    token,
+  });
+};
 
 export default createUserToken;
