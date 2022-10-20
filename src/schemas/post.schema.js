50c20035a598067587db6f364015e8d764869e3d
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-10-20 18:57:21 -0300

    feat: Added search

--- src/schemas/post.schema.js
+++ src/schemas/post.schema.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const postSchema = Joi.object({
   link: Joi.string().uri().required(),
-  description: Joi.string(),
+  description: Joi.string().allow(null, ""),
 });
 
 export { postSchema };
