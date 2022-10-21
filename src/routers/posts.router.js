cb0f25144581c4bb2d3f13ff929f0b0d6b1dedf6
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-10-21 18:00:05 -0300

    feat: added like route

--- src/routers/posts.router.js
+++ src/routers/posts.router.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { addHashtags } from "../controllers/hashtag.controller.js";
 import {
+  dislikePosts,
   getTimeline,
+  likePosts,
   newPost,
   tesLogin,
   testUser,
@@ -14,6 +16,8 @@ const router = Router();
 
 router.post("/posts", auth, schemaMiddleware(postSchema), addHashtags, newPost);
 router.get("/timeline", auth, getTimeline);
+router.post("/posts/like", auth, likePosts);
+router.post("/posts/dislike", auth, dislikePosts);
 
 router.post("/test/signin", testUser);
 router.post("/test/login", tesLogin);
