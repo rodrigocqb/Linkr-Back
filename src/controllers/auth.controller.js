d89ae973689c9ee99ea445dd285cfcd8926fe1f7
rodrigocqb <rodrigocortibarros@gmail.com>
2022-10-21 21:49:29 -0300

    test: removing test functions

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,3 @@
-import connection from "../database/database.js";
-
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -10,69 +8,54 @@ import { getUserByEmail, createUser } from "../repositories/auth.repository.js";
 import createUserToken from "../helpers/create-user-token.js";
 
 class AuthController {
+  static async signup(req, res) {
+    const { email, password, username, image } = req.body;
 
-    static async signup(req, res) {
+    // Verificando a existência do usuário
+    const userExists = await getUserByEmail(email);
 
-        const {
-            email,
-            password,
-            username,
-            image
-        } = req.body;
+    if (userExists.rows.length > 0)
+      return res.status(409).json({
+        message: "User already registered!",
+      });
 
-        // Verificando a existência do usuário
-        const userExists = await getUserByEmail(email);
+    // Criptografando a senha
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
 
-        if(userExists.rows.length > 0) return res.status(409).json({
-                message: "User already registered!"
-            }
-        );
+    try {
+      await createUser(email, passwordHash, username, image);
 
-        // Criptografando a senha
-        const salt = await bcrypt.genSalt(12);
-        const passwordHash = await bcrypt.hash(password, salt);
-
-        try {
-
-            await createUser(email, passwordHash, username, image);
-
-            return res.status(201).json({
-                message: "User created successfully!"
-            });
-
-        } catch (error) {
-            
-            return res.status(500).json({ message: error });
-
-        }
-
+      return res.status(201).json({
+        message: "User created successfully!",
+      });
+    } catch (error) {
+      return res.status(500).json({ message: error });
     }
+  }
 
-    static async signin(req, res) {
+  static async signin(req, res) {
+    const { email, password } = req.body;
 
-        const { email, password } = req.body;
+    // Verificando a existência do usuário
+    const userExists = await getUserByEmail(email);
 
-        // Verificando a existência do usuário
-        const userExists = await getUserByEmail(email);
+    if (!userExists.rows[0])
+      return res.status(401).json({
+        message: "E-mail not found!",
+      });
 
-        if(!userExists.rows[0]) return res.status(401).json({
-                message: "E-mail not found!"
-            }
-        );
+    const user = userExists.rows[0];
 
-        const user = userExists.rows[0];
+    const checkPassword = await bcrypt.compare(password, user.password);
 
-        const checkPassword = await bcrypt.compare(password, user.password);
+    if (!checkPassword)
+      return res.status(401).json({
+        message: "Invalid password!",
+      });
 
-        if(!checkPassword) return res.status(401).json({
-                message: "Invalid password!"
-            }
-        );
-
-        await createUserToken(user, req, res);
-
-    }
-
+    await createUserToken(user, req, res);
+  }
 }
 
 export default AuthController;
