3039dca9a50e5dcdd7fa0314cb81afce781c2fe5
rodrigocqb <rodrigocortibarros@gmail.com>
2022-10-21 21:59:05 -0300

    feat: Searching for username on db and adequating auth controller to helper file

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,58 +3,56 @@ dotenv.config();
 
 import bcrypt from "bcrypt";
 
-import { getUserByEmail, createUser } from "../repositories/auth.repository.js";
+import {
+  getUserByEmail,
+  createUser,
+  getUserByEmailOrUsername,
+} from "../repositories/auth.repository.js";
 
 import createUserToken from "../helpers/create-user-token.js";
+import {
+  conflictResponse,
+  createdResponse,
+  serverError,
+  unauthorizedResponse,
+} from "../helpers/controllers.helper.js";
 
 class AuthController {
   static async signup(req, res) {
     const { email, password, username, image } = req.body;
 
-    // Verificando a existência do usuário
-    const userExists = await getUserByEmail(email);
+    const userExists = await getUserByEmailOrUsername(email, username);
 
-    if (userExists.rows.length > 0)
-      return res.status(409).json({
-        message: "User already registered!",
-      });
+    if (userExists.rows.length > 0) return conflictResponse(res);
 
-    // Criptografando a senha
     const salt = await bcrypt.genSalt(12);
     const passwordHash = await bcrypt.hash(password, salt);
 
     try {
       await createUser(email, passwordHash, username, image);
 
-      return res.status(201).json({
-        message: "User created successfully!",
-      });
+      return createdResponse(res);
     } catch (error) {
-      return res.status(500).json({ message: error });
+      console.log(error.message);
+      return serverError(res);
     }
   }
 
   static async signin(req, res) {
     const { email, password } = req.body;
 
-    // Verificando a existência do usuário
     const userExists = await getUserByEmail(email);
 
-    if (!userExists.rows[0])
-      return res.status(401).json({
-        message: "E-mail not found!",
-      });
+    if (!userExists.rows[0]) return unauthorizedResponse(res);
 
     const user = userExists.rows[0];
 
     const checkPassword = await bcrypt.compare(password, user.password);
 
-    if (!checkPassword)
-      return res.status(401).json({
-        message: "Invalid password!",
-      });
+    if (!checkPassword) return unauthorizedResponse(res);
 
     await createUserToken(user, req, res);
+    return;
   }
 }
 
