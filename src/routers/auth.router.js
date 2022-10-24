39f1b7f843094816e98360965654780e2cc4fd24
rodrigocqb <rodrigocortibarros@gmail.com>
2022-10-23 21:56:23 -0300

    refactor: using generic middleware to validate login/signup schemas

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,13 +1,21 @@
 import { Router } from "express";
 
 import AuthController from "../controllers/auth.controller.js";
-
-import validateLogin from '../middlewares/validateLoginMiddleware.js';
-import validateRegister from '../middlewares/validateRegisterMiddleware.js';
+import { schemaMiddleware } from "../middlewares/schema.middleware.js";
+import loginSchema from "../schemas/login.schema.js";
+import registerSchema from "../schemas/register.schema.js";
 
 const authRouter = Router();
 
-authRouter.post("/signup", validateRegister, AuthController.signup);
-authRouter.post("/signin", validateLogin, AuthController.signin);
+authRouter.post(
+  "/signup",
+  schemaMiddleware(registerSchema),
+  AuthController.signup
+);
+authRouter.post(
+  "/signin",
+  schemaMiddleware(loginSchema),
+  AuthController.signin
+);
 
 export default authRouter;
