d8b2898ee9425baa244ab53ed4db45299f16d5af
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-10-25 20:36:23 -0300

    docs: created a new route to get the users followers

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -1,5 +1,6 @@
 import express from "express";
 import {
+  getFollowers,
   getUserPosts,
   getUsersBySearch,
 } from "../controllers/users.controller.js";
@@ -8,6 +9,7 @@ import { auth } from "../middlewares/authorization.middleware.js";
 const router = express.Router();
 
 router.get("/user/:id", auth, getUserPosts);
+router.get("/followers/user", auth, getFollowers);
 router.get("/search/:name", auth, getUsersBySearch);
 
 export default router;
