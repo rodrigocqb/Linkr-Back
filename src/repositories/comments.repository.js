3a6614360ebce2bbeef4b73cb047cac7679f053f
rodrigocqb <rodrigocortibarros@gmail.com>
2022-10-25 20:30:42 -0300

    feat: Added GET request for post's comments

--- src/repositories/comments.repository.js
+++ src/repositories/comments.repository.js
@@ -1,7 +1,18 @@
 import connection from "../database/database.js";
 
 const insertComment = async ({ userId, postId, comment }) => {
-    return await connection.query(`INSERT INTO comments (user_id, posts_id, comment) VALUES ($1, $2, $3);`, [userId, postId, comment])
-}
+  return await connection.query(
+    `INSERT INTO comments (user_id, posts_id, comment) VALUES ($1, $2, $3);`,
+    [userId, postId, comment]
+  );
+};
 
-export const commentRepository = { insertComment }
+const getComments = async (id) => {
+  await connection.query(
+    `SELECT id, user_id, post_id, comment
+    FROM comments WHERE post_id = $1`,
+    [id]
+  );
+};
+
+export const commentRepository = { insertComment, getComments };
