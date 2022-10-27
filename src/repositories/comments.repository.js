a8d301808ca0c63a490f60583b548be083de2269
Tallispt <tallispt@gmail.com>
2022-10-27 20:34:30 -0300

    fix: 'posts_id' comments table changed to post_id'

--- src/repositories/comments.repository.js
+++ src/repositories/comments.repository.js
@@ -2,15 +2,15 @@ import connection from "../database/database.js";
 
 const insertComment = async ({ userId, postId, comment }) => {
   return await connection.query(
-    `INSERT INTO comments (user_id, posts_id, comment) VALUES ($1, $2, $3);`,
+    `INSERT INTO comments (user_id, post_id, comment) VALUES ($1, $2, $3);`,
     [userId, postId, comment]
   );
 };
 
 const getComments = async (id) => {
   return await connection.query(
-    `SELECT id, user_id, posts_id, comment
-    FROM comments WHERE posts_id = $1`,
+    `SELECT id, user_id, post_id, comment
+    FROM comments WHERE post_id = $1`,
     [id]
   );
 };
